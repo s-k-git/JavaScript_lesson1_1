6adb13d899a676ce9d445c90ad9ffd0892180aff
s-k-git <89926320+s-k-git@users.noreply.github.com>
2021-09-03 01:54:18 +0900

    Update lesson1_1.js

--- lesson1_1.js
+++ lesson1_1.js
@@ -3,9 +3,9 @@ for (let i = 1; i < 100; i++) {
     const buzz = 5;
     if ((i % fizz === 0) && (i % buzz === 0)) {
         console.log("FizzBuzz " + i);
-    } else if (i % 3 === 0) {
+    } else if (i % fizz === 0) {
         console.log("Fizz " + i);
-    } else if (i % 5 === 0) {
+    } else if (i % buzz === 0) {
         console.log("Buzz " + i);
     }
 }
